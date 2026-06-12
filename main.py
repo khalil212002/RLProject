3569c5c52ed9c31e0246e2f18590f06faa1bd50f
Khillo <khalilwarwar2@gmail.com>
2026-06-12 15:42:29 +0300

    add create videos to main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from metrics_test import metricsTest
 from draw_metrics import draw_from_csv_compare_algorithm, draw_from_csv_compare_reward
 from sweep_test import sweepTest, draw_sweep_from_csv
+import create_animation
 
 
 def main():
@@ -9,6 +10,7 @@ def main():
     draw_from_csv_compare_reward()
     sweepTest()
     draw_sweep_from_csv()
+    create_animation.main()
 
 
 if __name__ == "__main__":
